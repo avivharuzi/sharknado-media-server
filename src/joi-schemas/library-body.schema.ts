55a26ab4d3419749e6936ca06ca5856600b9783d
Aviv Haruzi <avivharuzi@gmail.com>
2020-03-21 19:03:06 +0200

    Update library body schema

--- src/joi-schemas/library-body.schema.ts
+++ src/joi-schemas/library-body.schema.ts
@@ -1,6 +1,9 @@
 import * as Joi from 'joi';
 
+import { LibraryType } from '../modules/Library/Shared/LibraryType';
+
 export default Joi.object().keys({
   name: Joi.string().min(3).max(255).required(),
+  type: Joi.string().valid(LibraryType.Video, LibraryType.Audio, LibraryType.Photo).required(),
   paths: Joi.array().required(),
 });
