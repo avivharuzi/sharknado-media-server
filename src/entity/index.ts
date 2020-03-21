0fec08af6f1b493726c0455d75682665d64b8112
Aviv Haruzi <avivharuzi@gmail.com>
2020-03-21 17:27:46 +0200

    Add index.ts to entity models

---
+++ src/entity/index.ts
@@ -0,0 +1,11 @@
+import { Library } from './Library';
+import { Folder } from './Folder';
+import { File } from './File';
+import { Metadata } from './Metadata';
+
+export {
+  Library,
+  Folder,
+  File,
+  Metadata,
+};
