0f3a948cb69afa3a7589a1086853737ff45c4da8
Aviv Haruzi <avivharuzi@gmail.com>
2020-03-23 19:29:55 +0200

    Update slugify function

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,11 +1,15 @@
 export default (s: string): string => {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+  const p = new RegExp(a.split('').join('|'), 'g');
+
   return s
-    .toString()
-    .trim()
     .toLowerCase()
     .replace(/\s+/g, '-')
+    .replace(p, c => b.charAt(a.indexOf(c)))
+    .replace(/&/g, '-and-')
     .replace(/[^\w\-]+/g, '')
-    .replace(/--+/g, '-')
+    .replace(/\-\-+/g, '-')
     .replace(/^-+/, '')
     .replace(/-+$/, '');
 };
