84921cb1b134c0d105d8b6eed14c48d7feccf24a
Aviv Haruzi <avivharuzi@gmail.com>
2020-03-23 22:42:03 +0200

    Add apiRouter

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
-import { Application } from 'express';
+import { Application, Router } from 'express';
 
 import * as BrowseRouter from './modules/Browse/BrowseRouter';
 import * as LibraryRouter from './modules/Library/LibraryRouter';
 import * as StreamRouter from './modules/Stream/StreamRouter';
 
+const apiRouter: Router = Router();
+
+apiRouter.use('/browse', BrowseRouter.router);
+apiRouter.use('/stream', StreamRouter.router);
+apiRouter.use('/libraries', LibraryRouter.router);
+
 export default (app: Application) => {
-  app.use('/browse', BrowseRouter.router);
-  app.use('/stream', StreamRouter.router);
-  app.use('/libraries', LibraryRouter.router);
+  app.use('/api', apiRouter);
 };
